a34859f7c6860b3457a0330dc379434a22147886
Ajanitone <ajanitone@freenet.de>
2022-10-11 13:33:25 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import React from "react"; //Main React.js library
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 
 // This function returns a string that will be rendered
+
 export const PrintHello = () => {
-	return <h1>Hello World</h1>;
+  return <h1>I love React</h1>;
 };
 
 //              what           where
